19d5930df25a830d98387df8bb4afb4e48d45de0
Vinay <vinaybhaskar2000@yahoo.com>
2019-08-02 15:22:04 +0530

    Modifications are added

--- Mavenjava/src/test/java/MySoftwareCompany/RESTAPITest.java
+++ Mavenjava/src/test/java/MySoftwareCompany/RESTAPITest.java
@@ -8,6 +8,8 @@ public class RESTAPITest
   public void postJira()
   {
 	  System.out.println("postJira");  
+	  System.out.println("2nd August 2019 Friday");  
+	  System.out.println("first Sravana Sukravaram, Heavy rain from morning");  
   }
   @Test
   public void deleteTwitter()
